62b9e92e7bba82a88e57ee8b466c41f077847295
Florian Loch <me@fdlo.ch>
2021-01-06 22:24:16 +0100

    Rewrite QueryBuilder in order to use OpenAPI annotations properly. Annotate Album route

--- src/main/kotlin/ch/fdlo/hoerbuchspion/webservice/App.kt
+++ src/main/kotlin/ch/fdlo/hoerbuchspion/webservice/App.kt
@@ -1,7 +1,12 @@
 package ch.fdlo.hoerbuchspion.webservice
 
+import ch.fdlo.hoerbuchspion.webservice.data.Album
+import ch.fdlo.hoerbuchspion.webservice.data.AlbumDetails
+import ch.fdlo.hoerbuchspion.webservice.data.Artist
+import ch.fdlo.hoerbuchspion.webservice.data.PaginationWrapper
 import ch.fdlo.hoerbuchspion.webservice.db.QueryBuilder
 import ch.fdlo.hoerbuchspion.webservice.db.queryCrawlStats
+import io.jooby.Context
 import io.jooby.Kooby
 import io.jooby.hibernate.HibernateModule
 import io.jooby.hikari.HikariModule
@@ -9,11 +14,27 @@ import io.jooby.json.JacksonModule
 import io.jooby.runApp
 import io.jooby.whoops.WhoopsModule
 import io.jooby.OpenAPIModule
+import io.jooby.annotations.QueryParam
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import java.lang.IllegalArgumentException
+import java.lang.Long
+import javax.persistence.EntityManager
+import kotlin.math.min
 
-
-
-
-
+@OpenAPIDefinition(
+    info = Info(
+        title = "Audiobooks-On-Spotify",
+        description = "Providing information on audiobooks available on Spotify to make it easier for you to find good books to listen to.",
+    )
+)
 class App : Kooby({
     // The implicit route "/swagger/" provides OpenAPI documentation
     install(OpenAPIModule())
@@ -27,18 +48,114 @@ class App : Kooby({
 
     install(WhoopsModule())
 
-    get("/albums") { it ->
-        QueryBuilder.fetchAlbums(it)
-    }
+    get("/albums", ::getAlbums)
 
-    get("/artists") { it ->
-        QueryBuilder.fetchArtists(it)
-    }
+    get("/artists", ::getArtists)
 
     get("/stats") { it ->
         queryCrawlStats(it)
     }
 })
+
+@Operation(
+    summary = "Provides albums/audiobooks matching given filters. Information is wrapped inside a PaginationWrapper object.",
+    method = "GET",
+    responses = [
+        ApiResponse(
+            responseCode = "400",
+            description = "{\n" +
+                    "  \"message\": \"Error description\",\n" +
+                    "  \"status\": 400,\n" +
+                    "  \"reason\": \"Some details on the error.\"\n" +
+                    "}"
+        ),
+        ApiResponse(
+            responseCode = "default",
+            description = "{\"total\":1,\"offset\":0,\"limit\":50,\"items\":[{\"id\":\"990023ace67a\",\"name\":\"Fancy Album\",\"artist\":{\"id\":\"23129390abdc\",\"name\":\"Some Super Fancy Artist\",\"artistImage\":\"http://artist.com/image.png\",\"popularity\":90},\"releaseDate\":\"2020-08-01\",\"albumArtUrl\":\"http://albumart.de/image1.png\",\"albumType\":\"ALBUM\",\"storyType\":\"UNABRIDGED\",\"totalTracks\":10,\"totalDurationMs\":9078934,\"allTracksNotExplicit\":true,\"allTracksPlayable\":true,\"previewURL\":\"http://previewurl.de/sample.mp3\",\"popularity\":80,\"label\":\"Some Fancy Label\",\"copyright\":\"Copyright owner 1\",\"assumedLanguage\":\"DE\"}]}"
+        )],
+    parameters = [
+        Parameter(
+            name = "offset",
+            description = "Required for pagination. Position from which to start returning records.",
+            `in` = ParameterIn.QUERY,
+            required = false,
+            schema = Schema(
+                defaultValue = "0",
+                type = "integer",
+                minimum = "0"
+            )
+        ),
+        Parameter(
+            name = "limit",
+            description = "Required for pagination. Number of records to return starting from 'offset'.",
+            `in` = ParameterIn.QUERY,
+            required = false,
+            schema = Schema(
+                defaultValue = QueryBuilder.DEFAULT_ROW_LIMIT_PER_REQUEST.toString(),
+                type = "integer",
+                minimum = QueryBuilder.MIN_ROWS_REQUIRED_TO_BE_REQUESTED.toString(),
+                maximum = QueryBuilder.DEFAULT_ROW_LIMIT_PER_REQUEST.toString()
+            )
+        ),
+        Parameter(
+            name = "s",
+            description = "Search term records need to match.",
+            `in` = ParameterIn.QUERY,
+            required = false,
+            schema = Schema(
+                defaultValue = "%",
+                type = "string"
+            )
+        ),
+        Parameter(
+            name = "unabridged_only",
+            description = "Flag whether to provide only unabridged albums/audiobooks.",
+            `in` = ParameterIn.QUERY,
+            required = false,
+            schema = Schema(
+                defaultValue = "false",
+                type = "boolean"
+            )
+        ),
+        Parameter(
+            name = "language",
+            description = "Only provide albums/audiobooks matching the given language. Parameter can be set multiple times.",
+            `in` = ParameterIn.QUERY,
+            required = false,
+            schema = Schema(
+                type = "string",
+                allowableValues = ["de", "en", "fr", "es", "it"]
+            )
+        )]
+)
+fun getAlbums(ctx: Context): PaginationWrapper<Album> {
+    val offset = ctx.query("offset").longValue(0)
+    val limit = ctx.query("limit").longValue(QueryBuilder.DEFAULT_ROW_LIMIT_PER_REQUEST)
+    val searchTerm = ctx.query("s").value("%")
+    val unabridgedOnly = ctx.query("unabridged_only").booleanValue(false)
+    val languages = ctx.query("language").map {
+        val langStr = it.value().toLowerCase()
+        val lang = AlbumDetails.Language.fromISO_639_1(langStr)
+
+        if (lang == AlbumDetails.Language.UNKNOWN) {
+            throw IllegalArgumentException("'$langStr' is not an ISO 639-1 identifier or does not denote a supported language.")
+        }
+
+        lang
+    }.toSet()
+    val em = ctx.require(EntityManager::class.java)
+
+    return QueryBuilder.fetchAlbums(em, offset, limit, searchTerm, unabridgedOnly, languages)
+}
+
+fun getArtists(ctx: Context): PaginationWrapper<Artist> {
+    val offset = ctx.query("offset").longValue(0)
+    val limit = ctx.query("limit").longValue(QueryBuilder.DEFAULT_ROW_LIMIT_PER_REQUEST)
+    val searchTerm = ctx.query("s").value("%")
+    val em = ctx.require(EntityManager::class.java)
+
+    return QueryBuilder.fetchArtists(em, offset, limit, searchTerm)
+}
 
 fun main(args: Array<String>) {
     runApp(args, App::class)
--- src/main/kotlin/ch/fdlo/hoerbuchspion/webservice/db/QueryBuilder.kt
+++ src/main/kotlin/ch/fdlo/hoerbuchspion/webservice/db/QueryBuilder.kt
@@ -3,38 +3,63 @@ package ch.fdlo.hoerbuchspion.webservice.db
 import ch.fdlo.hoerbuchspion.webservice.data.*
 import com.querydsl.jpa.impl.JPAQuery
 import io.jooby.Context
-import io.jooby.require
 import javax.persistence.EntityManager
 
 import ch.fdlo.hoerbuchspion.webservice.data.QAlbum.album
 import ch.fdlo.hoerbuchspion.webservice.data.QArtist.artist
 import com.querydsl.core.types.Predicate
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.core.types.dsl.EntityPathBase
+import com.querydsl.core.types.dsl.StringPath
 import java.lang.IllegalArgumentException
 import kotlin.math.min
 
 class QueryBuilder {
     companion object {
         const val DEFAULT_ROW_LIMIT_PER_REQUEST = 50L
+        const val MIN_ROWS_REQUIRED_TO_BE_REQUESTED = 1L
 
-        fun fetchAlbums(ctx: Context): PaginationWrapper<Album> {
-            return fetchFromRequest(ctx, album)
+        fun fetchAlbums(
+            em: EntityManager,
+            offset: Long,
+            limit: Long,
+            searchTerm: String,
+            unabridgedOnly: Boolean,
+            languages: Set<AlbumDetails.Language>
+        ): PaginationWrapper<Album> {
+            var criteria = buildSearchTermPredicate(album.name, searchTerm)
+
+            if (unabridgedOnly) {
+                criteria = criteria.and(album.storyType.eq(Album.StoryType.UNABRIDGED))
+            }
+
+            if (languages.isEmpty().not()) {
+                criteria = criteria.and(album.albumDetails.assumedLanguage.`in`(languages))
+            }
+
+            return fetchFromRequest(em, album, criteria, offset, limit)
         }
 
-        fun fetchArtists(ctx: Context): PaginationWrapper<Artist> {
-            return fetchFromRequest(ctx, artist)
+        fun fetchArtists(
+            em: EntityManager,
+            offset: Long,
+            limit: Long,
+            searchTerm: String,
+        ): PaginationWrapper<Artist> {
+            val criteria = buildSearchTermPredicate(artist.name, searchTerm)
+
+            return fetchFromRequest(em, artist, criteria, offset, limit)
         }
 
         private fun <T> fetchFromRequest(
-            ctx: Context,
-            from: EntityPathBase<T>
+            em: EntityManager,
+            from: EntityPathBase<T>,
+            criteria: Predicate,
+            offset: Long,
+            limit: Long
         ): PaginationWrapper<T> {
-            val offset = ctx.query("offset").longValue(0)
-            val limit = min(ctx.query("limit").longValue(DEFAULT_ROW_LIMIT_PER_REQUEST), DEFAULT_ROW_LIMIT_PER_REQUEST)
+            val limit = min(limit, DEFAULT_ROW_LIMIT_PER_REQUEST).coerceAtLeast(MIN_ROWS_REQUIRED_TO_BE_REQUESTED)
 
-            val em = ctx.require(EntityManager::class)
-
-            val criteria = buildFilter(ctx, from)
             val count = JPAQuery<Long>(em).from(from).where(criteria).fetchCount()
 
             val records = if (count > 0 && offset < count)
@@ -46,36 +71,8 @@ class QueryBuilder {
             return PaginationWrapper(count, offset, limit, records)
         }
 
-        private fun <T> buildFilter(
-            ctx: Context,
-            from: EntityPathBase<T>
-        ): Predicate {
-            val searchString = "%${ctx.query("s").value("%").trim()}%"
-
-            return when (from) {
-                is QAlbum -> {
-                    val unabridgedOnly = ctx.query("unabridged_only").booleanValue(false)
-                    val languages = toLanguageSet(ctx.query("language").toSet(String::class.java))
-
-                    var predicate = from.name.likeIgnoreCase(searchString)
-
-                    if (unabridgedOnly) {
-                        predicate = predicate.and(from.storyType.eq(Album.StoryType.UNABRIDGED))
-                    }
-
-                    if (languages.isEmpty().not()) {
-                        predicate = predicate.and(from.albumDetails.assumedLanguage.`in`(languages))
-                    }
-
-                    predicate
-                }
-                is QArtist -> {
-                    from.name.likeIgnoreCase(searchString)
-                }
-                else -> {
-                    throw Throwable("Entity needs to be of either QAlbum or QArtist.")
-                }
-            }
+        private fun buildSearchTermPredicate(nameField: StringPath, searchTerm: String): BooleanExpression {
+            return nameField.likeIgnoreCase("%${searchTerm.trim()}%")
         }
 
         private fun toLanguageSet(stringSet: Set<String>): Set<AlbumDetails.Language> {
