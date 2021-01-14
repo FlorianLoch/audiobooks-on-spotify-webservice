f2f64985be3e9578a56f0d1dd0bb925b3f647880
Florian Loch <me@fdlo.ch>
2021-01-14 13:15:09 +0100

    Update webapp dependencies. Format config file.

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -17,11 +17,11 @@ module.exports = {
   },
   configureWebpack: {
     plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                PACKAGE_VERSION: `"${version}"`
-            }
-        })
+      new webpack.DefinePlugin({
+        "process.env": {
+          PACKAGE_VERSION: `"${version}"`
+        }
+      })
     ]
-},
+  }
 }
